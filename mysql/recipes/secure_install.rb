9adc612c8f45b4b14a146bd8c03df3c35165362a
anamika2103 <anamika.kumari@opstree.com>
2017-08-03 22:14:03 +0530

    Update secure_install.rb

--- mysql/recipes/secure_install.rb
+++ mysql/recipes/secure_install.rb
@@ -1,13 +1,10 @@
 bash 'user-create' do
   code <<-EOH
-  mysql -u root -panamika -e "CREATE DATABASE #{node['mysql']['db_name']};"
-  mysql -u root -panamika -e "CREATE USER #{node['mysql']['name']} IDENTIFIED BY #{node['mysql']['password']};"
-  mysql -u root -panamika -e "GRANT ALL ON #{node['mysql']['db_name']}.* TO \"#{node['mysql']['name']}\";"
-  mysql -u root -panamika -e "FLUSH PRIVILEGES;"
+  mysql -u root -e "CREATE DATABASE #{node['mysql']['db_name']};"
+  mysql -u root -e "CREATE USER #{node['mysql']['name']} IDENTIFIED BY #{node['mysql']['password']};"
+  mysql -u root -e "GRANT ALL ON #{node['mysql']['db_name']}.* TO \"#{node['mysql']['name']}\";"
+  mysql -u root -e "FLUSH PRIVILEGES;"
  EOH
 end
-
-#mysql -u root -panamika -e "CREATE USER \'#{node['mysql']['name']}\' IDENTIFIED BY \'#{node['mysql']['password']}\';"
-#mysql -u root -panamika -e "GRANT ALL PRIVILEGES ON *.* TO \'#{node['mysql']['name']}\';"
 
 
